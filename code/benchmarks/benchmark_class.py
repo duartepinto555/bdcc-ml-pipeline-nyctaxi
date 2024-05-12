60835acf58cc9fabd23318d049a3491823b3badf
duartepinto555 <duarte.pinto7337@gmail.com>
2024-05-12 16:35:42 +0200

    Created "main.py" and started using "benchmark_class" (also changed benchmark class a bit)

--- code/benchmarks/benchmark_class.py
+++ code/benchmarks/benchmark_class.py
@@ -1,47 +1,63 @@
 import os
 import time
+import logging
 import numpy as np
 import pandas as pd
 import modin.pandas as mpd
 import dask.dataframe as dd
 import cudf # RapidsAI
 import dask_cudf
+
+from typing import Literal
 from joblib import Parallel, delayed
 
 
-class dataframe_benchmark:
+class Benchmark:
 
-    def __init__(self, file_dir, engine='pandas', df_type='pandas'):
+    def __init__(
+            self,
+            file_dir,
+            engine: Literal['pandas', 'modin', 'dask', 'cudf', 'joblib']='pandas',
+            df_type: Literal['pandas', 'dask']='pandas'
+        ):
         self.engine = engine # engine can be 'pandas', 'modin', 'dask', 'cudf', or 'joblib'
-        self.df_type = df_type # df_type can be 'pandas', 'dask', 'cudf', or 'joblib'
+        self.df_type = df_type # df_type can be 'pandas', 'dask'
+        self.file_dir = file_dir
         self.exec_times = {}
+        # Default "engine"
+        self.pd = pd
         if engine == 'modin':
             os.environ["MODIN_ENGINE"] = "dask"  # Modin will use Dask
             self.pd = mpd
-            self.df = self.pd.read_parquet(file_dir, index='index')
         elif engine == 'dask':
-            self.df = dd.read_parquet(file_dir, index='index')
+            self.pd = dd
         elif engine == 'cudf': # we can use cudf or dd 
             if df_type == 'dask':
-                self.df = dask_cudf.read_parquet(file_dir, index='index')
+                self.pd = dask_cudf
             else:
-                self.df = cudf.read_parquet(file_dir, index='index')
+                self.pd = cudf
         elif engine == 'joblib':
             self.pd = pd
-            self.df = self.pd.read_parquet(file_dir, index='index')
-        else:
-            self.pd = pd
-            self.df = self.pd.read_parquet(file_dir, index='index')
+
+        self.df = self.pd.read_parquet(self.file_dir, index='index')
+        self.other = self.pd.DataFrame(self.groupby_statistics(self.df).to_pandas())
+        self.other.columns = pd.Index([e[0]+'_' + e[1] for e in self.other.columns.tolist()])
 
     def time_decorator(self, func):
         def wrapper(*args, **kwargs):
             start_time = time.time()
+            logging.info(f'Starting {func.__name__}')
             func(*args, **kwargs)
+            logging.info(f'Finished {func.__name__}')
             end_time = time.time()
             execution_time = end_time - start_time
             self.exec_times[func.__name__] = execution_time
         return wrapper
     
+    @time_decorator
+    def read_file_parquet(self):
+        return self.pd.read_parquet(self.file_dir, index='index')
+
     @time_decorator
     def count(self):
         if self.engine in ['dask', 'cudf']:
@@ -162,3 +178,31 @@ class dataframe_benchmark:
             return result.compute()
         else:
             return result
+    
+    @time_decorator
+    def join_count(self):
+        if self.engine == 'dask':
+            return len(self.pd.merge(self.df, self.other, left_index=True, right_index=True))
+    
+    @time_decorator
+    def join_data(self):
+        if self.engine == 'dask':
+            return self.pd.merge(self.df, self.other, left_index=True, right_index=True).compute()
+
+    def run_benchmark(self):
+        self.read_file_parquet()
+        self.count()
+        self.count_index_length()
+        self.mean()
+        self.standard_deviation()
+        self.mean_of_sum()
+        self.sum_columns()
+        self.mean_of_product()
+        self.product_columns()
+        self.value_counts()
+        self.mean_of_complicated_arithmetic_operation()
+        self.complicated_arithmetic_operation()
+        self.groupby_statistics()
+        self.join_count()
+        self.join_data()
+
---
+++ code/main.py
@@ -0,0 +1,34 @@
+import datetime as dt
+import download_taxi_datasets
+from benchmarks.benchmark_class import Benchmark
+
+
+def main():    
+    # Which years we wan't to fetch the data from
+    years = range(2009, 2010)
+    date_range = [dt.datetime(year, month, 1) for year in years for month in range(1, 13)]
+
+    # To which directory do we wish to save the data in 
+    output_folder = '/'.join(__file__.split('/')[:-2]) + '/datasets'
+    download_taxi_datasets.download_taxi_files(date_range, output_folder)
+
+    # Run benchmarks for all engines
+    engine_configs = [
+        {'engine': 'pandas', 'df_type': 'pandas'},
+        {'engine': 'modin', 'df_type': 'pandas'},
+        {'engine': 'dask', 'df_type': 'pandas'},
+        {'engine': 'joblib', 'df_type': 'pandas'},
+        {'engine': 'cudf', 'df_type': 'dask'},
+        {'engine': 'cudf', 'df_type': 'pandas'},
+    ]
+    benchmarks = []
+    for engine_config in engine_configs:
+        benchmarks.append(Benchmark(
+            file_dir=f'{output_folder}/ks_yellow_taxi_tripdata_2009-01.parquet',
+            **engine_config
+        ))
+        benchmarks[-1].run_benchmark()
+
+
+if __name__ == '__main__':
+    main()
